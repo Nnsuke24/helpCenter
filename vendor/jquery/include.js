9c43243fdc8a36bc216ceed7de2847faadd214a7
kosukefukuda <nnsuke24@gmail.com>
2017-04-09 19:18:32 +0900

    include実装
    ヘッダーとフッター箇所をjQueryを使用してincludeするように変更

---
+++ vendor/jquery/include.js
@@ -0,0 +1,10 @@
+$(function() {
+	// $.ajaxSetup({cache:false}); //なくても良いがキャッシュを読み込まないように?
+	$('#header').load('header.html');
+});
+
+$(function() {
+	// $.ajaxSetup({cache:false}); //なくても良いがキャッシュを読み込まないように?
+	$('#footer').load('footer.html');
+});
+
